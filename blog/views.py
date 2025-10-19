32029cffaef04c8386dee79adfa3ed51b36aadc5
Sharjeel Ahmed <142432342+sharjeel-ahmed17@users.noreply.github.com>
2025-10-20 01:48:26 +0500

    123

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,30 @@
+# from django.shortcuts import render
+# from .models import DashboardItem
+
+# def dashboard(request):
+#     # items = DashboardItem.objects.all().order_by('-created_at')
+#     edr_data = [
+#         {"edr_no": 4426467, "unit_id": 11052051, "batch": 223280, "rtc_time": "10/9/2025 2:41:23 AM", "di": 2, "ai": 33,
+#          "speed": 67.98, "x_value": 24.68, "y_value": 3, "gsm_signals": 428, "n_value": 169.7, "qty": "✓"},
+#     ]
+#     return render(request, 'blog/dashboard.html', {'edr_data': edr_data})
+
+
+
+
+# # def edr_dashboard(request):
+# #     # example static data (replace with queryset or API data)
+# #     edr_data = [
+# #         {"edr_no": 4426467, "unit_id": 11052051, "batch": 223280, "rtc_time": "10/9/2025 2:41:23 AM", "di": 2, "ai": 33,
+# #          "speed": 67.98, "x_value": 24.68, "y_value": 3, "gsm_signals": 428, "n_value": 169.7, "qty": "✓"},
+# #     ]
+# #     return render(request, 'edr_dashboard.html', {'edr_data': edr_data})
+
+
+
 from django.shortcuts import render
 
-def edr_dashboard(request):
+def dashboard(request):
     edr_data = [
         {"edr_no": 4426467, "unit_id": 11052051, "batch": 223280, "rtc_time": "10/9/2025 2:41:23 AM",
          "di": 2, "ai": 33, "speed": 67.98, "x_value": 24.68, "y_value": 3,
@@ -10,4 +34,4 @@ def edr_dashboard(request):
          "gsm_signals": 425, "n_value": 169.0, "qty": "✓"},
         # Add more sample rows or load from DB
     ]
-    return render(request, 'edr_dashboard.html', {'edr_data': edr_data})
+    return render(request, 'blog/dashboard.html', {'edr_data': edr_data})
