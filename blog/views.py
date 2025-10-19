c2e1d9a2a150aca2f9653350f43fab820a78d17c
Sharjeel Ahmed <142432342+sharjeel-ahmed17@users.noreply.github.com>
2025-10-20 01:49:49 +0500

    add dummy data

--- blog/views.py
+++ blog/views.py
@@ -32,6 +32,18 @@ def dashboard(request):
         {"edr_no": 4426468, "unit_id": 11052051, "batch": 223280, "rtc_time": "10/9/2025 2:41:33 AM",
          "di": 2, "ai": 34, "speed": 67.99, "x_value": 24.68, "y_value": 3,
          "gsm_signals": 425, "n_value": 169.0, "qty": "✓"},
+        {"edr_no": 4426468, "unit_id": 11052051, "batch": 223280, "rtc_time": "10/9/2025 2:41:33 AM",
+         "di": 2, "ai": 34, "speed": 67.99, "x_value": 24.68, "y_value": 3,
+         "gsm_signals": 425, "n_value": 169.0, "qty": "✓"},
+        {"edr_no": 4426468, "unit_id": 11052051, "batch": 223280, "rtc_time": "10/9/2025 2:41:33 AM",
+         "di": 2, "ai": 34, "speed": 67.99, "x_value": 24.68, "y_value": 3,
+         "gsm_signals": 425, "n_value": 169.0, "qty": "✓"},
+        {"edr_no": 4426468, "unit_id": 11052051, "batch": 223280, "rtc_time": "10/9/2025 2:41:33 AM",
+         "di": 2, "ai": 34, "speed": 67.99, "x_value": 24.68, "y_value": 3,
+         "gsm_signals": 425, "n_value": 169.0, "qty": "✓"},
+        {"edr_no": 4426468, "unit_id": 11052051, "batch": 223280, "rtc_time": "10/9/2025 2:41:33 AM",
+         "di": 2, "ai": 34, "speed": 67.99, "x_value": 24.68, "y_value": 3,
+         "gsm_signals": 425, "n_value": 169.0, "qty": "✓"},
         # Add more sample rows or load from DB
     ]
     return render(request, 'blog/dashboard.html', {'edr_data': edr_data})
